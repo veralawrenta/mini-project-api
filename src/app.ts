9a88d8fe259b00932861bf28e29aee1c0dafc743
Javiernatfer <javiernatfer6942@gmail.com>
2025-11-26 16:04:49 +0700

    setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
+import cors from "cors";
 import express, { Express } from "express";
-import { PORT } from './config/env';
-import cors from 'cors';
-import {SampleRouter} from './modules/sample/sample.router';
+import "reflect-metadata";
+import { PORT } from "./config/env";
 import { errorMiddleware } from "./middlewares/error.middleware";
+import { SampleRouter } from "./modules/sample/sample.router";
 
 export class App {
   app: Express;
 
-  constructor() {    // constructor akan jalan setelah kita initialize class app
+  constructor() {
+    // constructor akan jalan setelah kita initialize class app
     this.app = express();
     this.configure();
     this.routes();
@@ -17,22 +19,22 @@ export class App {
     // ini hanya digunakan disini saja jadi dibuat private
     this.app.use(express.json());
     this.app.use(cors());
-    this.handleError()
+    this.handleError();
   }
 
-  private routes (){
+  private routes() {
     const sampleRouter = new SampleRouter();
 
-    this.app.use("/samples", sampleRouter.getRouter())
-
+    this.app.use("/samples", sampleRouter.getRouter());
   }
 
-  private handleError(){
+  private handleError() {
     this.app.use(errorMiddleware);
   }
 
   start() {
-    this.app.listen(PORT, () => 
-    console.log (`Server running on port : ${PORT}`))
+    this.app.listen(PORT, () =>
+      console.log(`Server running on port : ${PORT}`)
+    );
   }
 }
